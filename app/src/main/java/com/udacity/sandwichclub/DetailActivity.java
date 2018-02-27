98185cc32ece0c51ec134943a5857cf82bddd4de
Nicholas Welna <welnanick@gmail.com>
2018-02-27 13:31:33 -0800

    master: App finished
    All logic is finished.

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -28,35 +28,43 @@ public class DetailActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         if (intent == null) {
+
             closeOnError();
+
         }
 
         int position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
         if (position == DEFAULT_POSITION) {
+
             // EXTRA_POSITION not found in intent
             closeOnError();
             return;
+
         }
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
         Sandwich sandwich = JsonUtils.parseSandwichJson(json);
         if (sandwich == null) {
+
             // Sandwich data unavailable
             closeOnError();
             return;
+
         }
 
         populateUI(sandwich);
         Picasso.with(this).load(sandwich.getImage()).into(ingredientsIv);
 
         setTitle(sandwich.getMainName());
+
     }
 
     private void closeOnError() {
 
         finish();
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
+
     }
 
     private void populateUI(Sandwich sandwich) {
@@ -85,5 +93,51 @@ public class DetailActivity extends AppCompatActivity {
 
         }
 
+        TextView originTv = findViewById(R.id.origin_tv);
+        String origin = sandwich.getPlaceOfOrigin();
+        if (origin != null && !origin.equals("")) {
+
+            originTv.setText(origin);
+
+        }
+        else {
+
+            originTv.setText(R.string.origin_unknown);
+
+        }
+
+        TextView descriptionTv = findViewById(R.id.description_tv);
+        String description = sandwich.getDescription();
+        if (description != null && !description.equals("")) {
+
+            descriptionTv.setText(description);
+
+        }
+
+        TextView ingredientsTv = findViewById(R.id.ingredients_tv);
+        List<String> ingredients = sandwich.getIngredients();
+        if (ingredients.size() == 0) {
+
+            ingredientsTv.setText(R.string.aka_not_applicable);
+
+        }
+        else {
+
+            StringBuilder ingredientsStringBuilder = new StringBuilder();
+            for (String ingredient : ingredients) {
+
+                if (ingredientsStringBuilder.length() != 0) {
+
+                    ingredientsStringBuilder.append(", ");
+
+                }
+                ingredientsStringBuilder.append(ingredient);
+
+            }
+            ingredientsTv.setText(ingredientsStringBuilder.toString());
+
+        }
+
     }
+
 }
